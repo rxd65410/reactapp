6107b769049fbd1e26e7092f6d97c0a8c2d52d1f
RAVITEJA DUGGIRALA <dravi475@gmail.com>
2018-08-02 21:23:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+////change same line
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
